8e950d45b147d0c42393858cb7505cb318156df7
Phani Rithvij <phanirithvij2000@gmail.com>
2019-09-07 12:05:27 +0530

    git up

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-from parse_q import sqlparse
+from parse_q import read_metadata
+from parse_q import parse_query
+import sys
 
+
+class SQLEngine():
+    def __init__(self, file_name):
+        self.schema = read_metadata(file_name)
+
+
+if __name__ == '__main__':
+    engine = SQLEngine("files/metadata.txt")
+    print(engine.schema)
+    querylist = sys.argv[1:]
+    for tok in parse_query(';'.join(querylist)):
+        print(tok.tokens)
--- parse_q.py
+++ parse_q.py
@@ -1,6 +1,42 @@
 import sqlparse
 
 
-def read_metadata(filename="metadata.txt"):
-    with open(filename, 'r') as f:
-        print(f.read())
+def parse_query(query):
+    return sqlparse.parse(query)
+
+def read_metadata(file_name="files/metadata.txt") -> dict:
+    """
+    Reads the metadata from a file and returns a dictionary
+    """
+    data = {}
+    insert = False
+    table = None
+    with open(file_name, 'r') as f:
+        content = f.readlines()
+        for i, line in enumerate(content):
+            # strip off the newline
+            content[i] = line[:-1]
+            line = line[:-1]
+
+            if table is not None:
+                if table not in data:
+                    data[table] = []
+
+                if insert:
+                    data[table].append(line)
+
+            if line == '<begin_table>':
+                insert = True
+                table = content[i+1][:-1]
+            elif line == '<end_table>':
+                insert = False
+                table = None
+
+        for k, v in data.items():
+            del v[0], v[-1]
+
+    return data
+
+
+if __name__ == "__main__":
+    print(read_metadata())
